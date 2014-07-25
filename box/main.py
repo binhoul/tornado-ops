d76c4ad12ef7b2fe947ce1bca8151ea6db95ccd6
binhoul <saintli86@163.com>
2014-07-25 18:09:21 +0800

    Update main.py

--- box/main.py
+++ box/main.py
@@ -15,7 +15,7 @@ define("port", default=8000, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="blog database host")
 define("mysql_database", default="sunbox", help="blog database name")
 define("mysql_user", default="root", help="blog database user")
-define("mysql_password", default="fengmao", help="blog database password")
+define("mysql_password", default="fengmxx", help="blog database password")
 
 class Application(tornado.web.Application):
     def __init__(self):
